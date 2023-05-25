2df04546f9e368d39ebd11b196a7f80d6b02f352
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-05-25 13:08:14 -0700

    betty abeg

--- ran.c
+++ ran.c
@@ -1,8 +1,19 @@
+#include "shell.h"
+/**
+ * _strcat - concatenates strings
+ * @string1: a string
+ * @addition: an addition
+ * @final_string: a final string
+ * Return: int
+ */
+
 int _strcat(char *string1, const char *addition, char *final_string)
 {
+	char  *ptr;
+
 	if (string1 == NULL || final_string == NULL)
 		return (-1);
-	char* ptr = final_string;
+	ptr = final_string;
 	while (*string1 != '\0')
 	{
 		*ptr = *string1;
