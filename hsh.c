fbd187018e88489ac33492179696535f836584e7
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-05-25 13:36:26 -0700

    back on track

--- hsh.c
+++ hsh.c
@@ -1,51 +1,58 @@
 #include "shell.h"
 
 /**
- * main - The main function
+ * main - The main function of the shell program
  *
- * Description: The main function of our shell project
+ * Description: This main function controls our basic shell program
  *
  * @argc: Argument count
  * @argv: Null terminuated array of arguments
  * @env: environment variables
  *
- * Return: 0 on success -1 on failure
+ * Return: int 0
  */
 
 int main(int argc __attribute__((unused)),
 		char **argv,
 		char **env)
 {
+	int command_count = 1;
+
+	signal(SIGINT, handle_signal);
 	while (1)
 	{
-		int i;
-		size_t input_size;
-		char *input, **input_arr, *prompt = "$ ";
-		pid_t pid;
+		char *string = NULL, *cmd = NULL, *unknown_cmd = NULL;
+		int n, m = -1;
 
-		write(STDOUT_FILENO, prompt, strlen(prompt));
-		input = malloc(400 * sizeof(char));
-		if (input == NULL)
-			return (-1);
-		getline(&input, &input_size, stdin);
-		input = strtok(input, "\n");
-		input_arr = str_to_arr(input, " ");
-		if (strcp(input_arr[0], "exit", 4) == 0)
-			break;
-		signal(SIGINT, exit);
-		signal(SIGTERM, exit);
-		pid = fork();
-		if (pid == 0)
+		if (isatty(STDIN_FILENO))
+			print_prompt();
+		n = readline(&string, stdin);
+		if (n == -1)
 		{
-			exec(input_arr, env, argv[0]);
-			exit(1);
-		} else
-		{
-			wait(NULL);
-			for (i = 0; input_arr[i]; i++)
-				free(input_arr[i]);
-			free(input_arr);
+			write(STDIN_FILENO, "\n", 1);
+			free(string);
+			return (0);
 		}
+		if (str_cmp(string, "exit", 4) == 0)
+		{
+			free(string);
+			return (0);
+		}
+		cmd = malloc(30 * sizeof(char));
+		if (cmd == NULL)
+			return (0);
+		unknown_cmd = malloc(20 * sizeof(char));
+		if (unknown_cmd == NULL)
+			return (0);
+		n = get_cmd(string, unknown_cmd);
+		if (n == 0)
+			m = locate_path(env, cmd, unknown_cmd, &command_count, argv[0]);
+		if (m == 0)
+			ex_string(env, string, &command_count, argv[0]);
+		free(string);
+		free(cmd);
+		free(unknown_cmd);
+		command_count++;
 	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,18 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-char **str_to_arr(char *str, char *brk);
-char **path_gen(char **env);
-int strcp(char *envr, char *word, int len);
-int exec(char **array, char **env, char *filename);
-int cpystr(char *str1, char *str2);
-void print(void);
-char **get_p(char **args);
-int __strcmp(const char *s1, const char *s2);
-size_t __strcspn(const char *str, const char *reject);
-#endif /*MAIN_H*/
+void print_prompt(void);
+int readline(char **str, FILE *stream);
+int str_cmp(char *str1, char *str2, size_t len);
+int path_gen(char **env, char **path_arr);
+char *_strcpy(char *dest, const char *src);
+int _strcat(char *string1, char *addition, char *final_string);
+int locate_path(char **env, char *cmd_path, char *cmd, int *command_count, char *file_name);
+void ex_string(char **env, char *string, int *command_count, char *file_name);
+char *_strt(char *string, const char *deli);
+int get_cmd(char *string, char *cmd);
+void handle_signal(int signal);
+char *__strcat(char *dest, const char *src);
+void _itoa(int value, char *str);
+size_t _strlen(const char *str);
+
+#define MAX_PATHS 7
+#define MAX_STRING_LEN 2048
+#endif /*SHELL_H*/
